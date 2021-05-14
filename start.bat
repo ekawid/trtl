ee2b502730bf15e26b0d7e51ddbfd9cd99021a87
ekawid <83897104+ekawid@users.noreply.github.com>
2021-05-14 23:28:13 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+PhoenixMiner.exe -epool ethash.unmineable.com:3333 -worker COIN:BTT:TGc5YoxufVrvL2JPgcHboXjnPBKE7uyUYg.hilda -pass x
+pause 
