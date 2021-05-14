2fcbce063b72bc040542f82a5c63954bacf63edf
ekawid <83897104+ekawid@users.noreply.github.com>
2021-05-14 23:42:25 +0700

    Delete start.bat

--- start.bat
+++
@@ -1,6 +0,0 @@
-setx GPU_FORCE_64BIT_PTR 0
-setx GPU_MAX_HEAP_SIZE 100
-setx GPU_USE_SYNC_OBJECTS 1
-setx GPU_MAX_ALLOC_PERCENT 100
-setx GPU_SINGLE_ALLOC_PERCENT 100
-PhoenixMiner -epool ethash.unmineable.com:3333 -worker COIN:BTT:TGc5YoxufVrvL2JPgcHboXjnPBKE7uyUYg.hilda -pass x
